fe8e6101d06e2afd8143f0ffc3f49b2f08eae3ad
silver.c <silver.c@tc-gaming.com>
2020-09-11 11:03:12 +0800

    Updates

---
+++ precache-manifest.cf86b3e33e220df9b420aa5457f2f391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309917d1f218018a9dfe0e7ce65331f6",
+    "url": "/moneyNote/index.html"
+  },
+  {
+    "revision": "4b52b2fa9e63674423a5",
+    "url": "/moneyNote/static/css/2.62a2ab10.chunk.css"
+  },
+  {
+    "revision": "b0e79a2e1612177a363b",
+    "url": "/moneyNote/static/css/main.2dfc51bb.chunk.css"
+  },
+  {
+    "revision": "4b52b2fa9e63674423a5",
+    "url": "/moneyNote/static/js/2.b9b53643.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/moneyNote/static/js/2.b9b53643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e79a2e1612177a363b",
+    "url": "/moneyNote/static/js/main.315e8356.chunk.js"
+  },
+  {
+    "revision": "4f925dfd234b32f1b6f2",
+    "url": "/moneyNote/static/js/runtime-main.67d5b2f2.js"
+  }
+]);
